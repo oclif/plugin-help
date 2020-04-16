9aaa951003340979f685a7a383d550899eb6336a
Chad Carbert <chadcarbert@me.com>
2020-04-16 09:55:20 -0400

    Add disclaimer on test help class included in src

--- src/_test-help-class.ts
+++ src/_test-help-class.ts
@@ -1,4 +1,8 @@
-// used in tests for getHelpClass
+// `getHelpClass` tests require an oclif project for testing so
+// it is re-using the setup here to be able to do a lookup for
+// this sample help class file in tests, although it is not needed
+// for @oclif/plugin-help itself.
+
 import {HelpBase} from '.'
 
 export default class extends HelpBase  {
