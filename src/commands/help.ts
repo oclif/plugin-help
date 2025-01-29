e464572cb8396b53a3de0b13ecdc5a1f61f09d89
Mike Donnalley <mdonnalley@salesforce.com>
2025-01-29 13:24:50 -0700

    chore(W-17692101): eslint 9 (#888)

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,21 +4,18 @@ export default class HelpCommand extends Command {
   static args = {
     command: Args.string({description: 'Command to show help for.', required: false}),
   }
-
   static description = 'Display help for <%= config.bin %>.'
-
   static flags = {
     'nested-commands': Flags.boolean({
       char: 'n',
       description: 'Include all nested commands in the output.',
     }),
   }
-
   static strict = false
 
   async run(): Promise<void> {
     const {argv, flags} = await this.parse(HelpCommand)
-    const Help = await loadHelpClass(this.config);
+    const Help = await loadHelpClass(this.config)
     const help = new Help(this.config, {all: flags['nested-commands']})
     await help.showHelp(argv as string[])
   }
